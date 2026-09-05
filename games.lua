d3e8cc0e6b1074d6e6ba39aae7b9b6409ccb3188
undeited <obsidianfw@proton.me>
2026-09-05 21:37:01 +0000

    Update

--- games.lua
+++ games.lua
@@ -2,6 +2,6 @@ return {
     [142823291] = "games/mm2/init.lua",
     [12645083079] = "games/gsr/init.lua",
     [6116002492] = "games/fbr/init.lua",
-    [6961824067] = "games/ftap/test.lua",
+    [6961824067] = "games/ftap/init.lua",
     [3623096087] = "games/ml/init.lua",
 }
