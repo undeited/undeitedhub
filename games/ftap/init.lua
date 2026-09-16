353d40f66ecf1925159cd3706705c755ff939488
undeited <obsidianfw@proton.me>
2026-09-15 22:59:16 -0500

    Update init.lua

--- games/ftap/init.lua
+++ games/ftap/init.lua
@@ -210,9 +210,9 @@ end
 
 SafeLoad("games/ftap/esp.lua")
 SafeLoad("games/ftap/combat.lua")
+SafeLoad("games/ftap/blobman.lua")
 SafeLoad("games/ftap/antis.lua")
 SafeLoad("games/ftap/misc.lua")
-SafeLoad("games/ftap/blobman.lua")
 SafeLoad("shared/settings.lua")
 
 if _G.UNDEITEDHUB_STATES then
